1f825384a728d8a5c0f072a6f02f369c540e9238
underthbluemoon <underthebluemoon0@gmail.com>
2025-11-25 15:43:55 +0900

    251125 로그인 로직 트랜잭션 처리

--- server/app/sevices/auth.service.js
+++ server/app/sevices/auth.service.js
@@ -13,38 +13,45 @@ import db from '../models/index.js';
 
 //                    ↱ 유저가 보내는 데이터
 async function login(body) {
-  const { email, password } = body;
-
-  // email로 유저 정보 획득
-  const user = await userRepository.findByEmail(null, email);
-
-  // 유저 정보 존재 여부 체크
-  if(!user) {
-    throw myError('유저 정보 없음', NOT_REGISTERED_ERROR);
-  }
-
-  // 비밀번호 체크
-  //                      ↱ 유저가 준 값
-  //                                ↱ 데이터베이스에 있는 암호화 값
-  if(!bcrypt.compareSync(password, user.password)) {
-    throw myError('비밀번호 틀림', NOT_REGISTERED_ERROR);
-  }
-
-  // JWT 생성
-  // accessToken : 인증용
-  const accessToken = jwtUtil.generateAccessToken(user)
-  // refreshToken : 토큰 재발급용
-  const refreshToken = jwtUtil.generateRefreshToken(user);
-
-  // refreshToken 저장
-  user.refreshToken = refreshToken;
-  await userRepository.save(null, user);
-
-  return {
-    accessToken,
-    refreshToken,
-    user,
-  };
+  // 트랜잭션 처리 : 하나의 처리를 트랜잭션으로 묶음
+  // return await db.sequelize.transaction(async t => {
+  //   서비스 로직
+  // });
+  //                                   ↱ parameter
+  return await db.sequelize.transaction(async t => {
+    const { email, password } = body;
+  
+    // email로 유저 정보 획득
+    const user = await userRepository.findByEmail(t, email);
+  
+    // 유저 정보 존재 여부 체크
+    if(!user) {
+      throw myError('유저 정보 없음', NOT_REGISTERED_ERROR);
+    }
+  
+    // 비밀번호 체크
+    //                      ↱ 유저가 준 값
+    //                                ↱ 데이터베이스에 있는 암호화 값
+    if(!bcrypt.compareSync(password, user.password)) {
+      throw myError('비밀번호 틀림', NOT_REGISTERED_ERROR);
+    }
+  
+    // JWT 생성
+    // accessToken : 인증용
+    const accessToken = jwtUtil.generateAccessToken(user)
+    // refreshToken : 토큰 재발급용
+    const refreshToken = jwtUtil.generateRefreshToken(user);
+  
+    // refreshToken 저장
+    user.refreshToken = refreshToken;
+    await userRepository.save(t, user);
+  
+    return {
+      accessToken,
+      refreshToken,
+      user,
+    };
+  });
 }
 
 export default {

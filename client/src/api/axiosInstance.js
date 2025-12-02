6871d9df3992835c47295d066ab823dd95a762ac
underthbluemoon <underthebluemoon0@gmail.com>
2025-12-02 18:01:31 +0900

    251202 axiosInstance - 서버 요청 전 interceptor로 엑세스 토큰 만료시간 체크 및 재발급

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import dayjs from 'dayjs';
+import { reissueThunk } from '../store/thunks/authThunk.js';
+
+// store 저장용 변수
+let store = null;
+
+// store 주입용 함수 : Main.jsx -> axiosInstance.js
+export function injectStoreInAios(_store) {
+  store = _store;
+}
 
 // axios 인스턴스 생성         ↱ axios 인스턴스 생성
 const axiosInstance = axios.create({
@@ -12,4 +23,76 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+// ===== 서버 요청할 때마다 액세스 토큰 만료 체크
+//                                             ↱ request 객체의 옵션
+axiosInstance.interceptors.request.use(async (config) => {
+  // 재시도 막을 url
+  const noRetry = /^\/api\/auth\/reissue$/;
+  // store에서 state 가져옴
+  let { accessToken } = store.getState().auth;
+  
+  try {
+    //                       ↱ 무한루프 방지 : 요청 → 5분 미만 → 아래 로직 실행하면서 reissueThunk() 실행
+    //                                                         = /api/auth/reissue에 요청 → 이것도 요청이니까 해당 조건 다시 체크
+    //                                       → 또 조건(시간) 체크. 이미 5분 미만이었으니 조건 다시 부합 → 아래 로직 실행 → 무한 루프
+    //                                       핵심: "재발급 요청도 하나의 axios 요청이라서 interceptor를 거친다!"
+    // 1. 엑세스 토큰이 있고 `/api/auth/reissue` 가 아닌 경우
+    if(accessToken && !noRetry.test(config.url)) {
+      // 엑세스 토큰 만료 확인
+      //              ↱ 디코딩해서 claim (payload 안의 정보) 가져옴
+      const claims = jwtDecode(accessToken);
+      const now = dayjs().unix();
+      const expTime = dayjs.unix(claims.exp).add(-5, 'minute').unix();
+      
+      // 1-1. 엑세스 토큰의 만료시간이 5분 이하면
+      if(now >= expTime) {
+        // 리이슈 요청 후, 엑세스 토큰 새로 담기
+        console.log('만료 5분 이내 토큰 재발급');
+        const response = await store.dispatch(reissueThunk()).unwrap();
+        accessToken = response.data.accessToken;
+      }
+  
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  } catch (error) {
+    console.log('axios Interceptor : ', error)
+    // thunk에서 에러 처리로 넘어감
+    return Promise.reject(error);
+  }
+});
+
 export default axiosInstance;
+
+
+// ===== 서버에 요청 -> 1. 성공 / 2. 실패. 토큰 만료 -> 리이슈
+// axiosInstance.interceptors.response.use(
+//   // 성공일 때 처리
+//   res => res, 
+//   // 실패일 때 처리
+//   async(error) => {
+//     const originalRequest = error.config;
+//     const noRetryList = [
+//       '/api/auth/reissue',
+//       '/api/auth/login'
+//     ];
+
+//     if(error.response?.status === 401 && !originalRequest._retry && !noRetryList.includes(originalRequest)) {
+//       originalRequest._retry = true;  // 재시도의 경우 조건에 걸리지 않게 (무한 루프 방지)
+//       try {
+//         const response = await store.dispatch(reissueThunk()).unwrap();
+
+//         if(response.data.accessToken) {
+//           originalRequest.headers['Authorizaton'] = `Bearer ${response.data.accessToken}`;
+//           return axiosInstance(originalRequest);
+//         } else {
+//           throw new Error('재발급실패');
+//         }
+//       } catch (error) {
+//         return Promise.reject(error);
+//       }
+//     }
+//     return Promise.reject(error)
+//   }
+// );

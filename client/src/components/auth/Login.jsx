b8bfa2bfdd75fa3b73b0fdc73b4618de5bc9ebc6
underthebluemoon <underthebluemoon0@gamil.com>
2025-12-06 22:58:00 +0900

    251206 [client] registration

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -47,6 +47,10 @@ export default function Login() {
     window.location.replace(`/api/auth/social/${provider}`)
   }
 
+  function redirectRegistration() {
+    return navigate('/registration');
+  }
+
   return (
     <>
     {email} / {password}
@@ -63,7 +67,7 @@ export default function Login() {
         <button type="submit" className="btn-big bg-gray">Log in</button>
         <div className="text-on-line">or</div>
         <button type="button" className="btn-big bg-img-kakao" onClick={() => { handleSocial('kakao') }}></button>
-        <button type="button" className="btn-big bg-light">Sign up</button>
+        <button type="button" className="btn-big bg-light" onClick={redirectRegistration}>Sign up</button>
       </form>
     </>
   )
